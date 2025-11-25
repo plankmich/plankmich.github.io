152c4bcb19a81da1675ed3335452695fd1f8c0c5
plankmich <plank.mich@gmail.com>
2025-11-25 14:22:58 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,270 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pan_7---panorama---kopie---kopie---kopie",
+      "name": "Pan_7 - Panorama - Kopie - Kopie - Kopie",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.020463555056176475,
+          "pitch": 0.12243566135051509,
+          "rotation": 0,
+          "target": "1-pan_1---panorama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-pan_1---panorama",
+      "name": "Pan_1 - Panorama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7923687576639615,
+          "pitch": 0.14924583164189542,
+          "rotation": 0,
+          "target": "2-pan_2---panorama---kopie"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-pan_2---panorama---kopie",
+      "name": "Pan_2 - Panorama - Kopie",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1106523858624655,
+          "pitch": 0.2581348837564601,
+          "rotation": 0,
+          "target": "3-pan_3---panorama---kopie---kopie"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-pan_3---panorama---kopie---kopie",
+      "name": "Pan_3 - Panorama - Kopie - Kopie",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-pan_4---panorama---kopie---kopie---kopie",
+      "name": "Pan_4 - Panorama - Kopie - Kopie - Kopie",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.500062617522337,
+          "pitch": 0.06622876609445072,
+          "rotation": 0,
+          "target": "5-pan_5---panorama---kopie---kopie---kopie"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-pan_5---panorama---kopie---kopie---kopie",
+      "name": "Pan_5 - Panorama - Kopie - Kopie - Kopie",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6379514513315598,
+          "pitch": 0.026134135372402056,
+          "rotation": 0,
+          "target": "6-pan_6---panorama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-pan_6---panorama",
+      "name": "Pan_6 - Panorama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0147204955198985,
+          "pitch": 0.023083306765240508,
+          "rotation": 0,
+          "target": "0-pan_7---panorama---kopie---kopie---kopie"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
